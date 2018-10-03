ad576c132e0dcd9d0ce6fd378c5f9429c18ef26c
akaswen <aaronofalfheim@gmail.com>
2018-10-03 16:56:27 +0200

    refactoring into modules and factories

--- script.js
+++ script.js
@@ -2,16 +2,59 @@ const player = (name, symbol) => {
     return {name, symbol};
 };
 
-const game = () => {
-    let player1 = player('Kasia', 'X');
-    let player2 = player('Aaron', 'O');
-    let winner;
+const board = (() => {
     let boardArray = [];
-    for (i=0; i<9; i++) {
-        boardArray.push(undefined);
-    }
     let board = document.getElementById('board');
 
+    const init = () => {
+        boardArray = [];
+        for (i=0; i<9; i++) {
+            boardArray.push(undefined);
+        }
+        board.addEventListener('click', game.makeMove);
+    };
+
+    const draw = (symbol, square) => {
+        let index = square.id
+        boardArray[index] = symbol;
+        square.textContent = symbol;
+    };
+
+    const checkEndGame = () => {
+        let winPositions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+        let gameOver = false;
+        winPositions.forEach((array) => {
+            let values = [];
+            array.forEach((n) => {
+                values.push(boardArray[n]);
+            });
+            if (values.every(x => x === 'X') || values.every(x => x === 'O')) {
+                //drawLine(array);
+                gameOver = true;
+            }
+        });
+        return gameOver;
+    };
+
+    const endGame = winner => {
+        board.removeEventListener('click', game.makeMove);
+        let heading = document.createElement('H1');
+        let container = document.querySelector('div.container');
+        if (winner) {
+            heading.textContent = winner.name;
+        } else {
+            heading.textContent = "Cat's game...";
+        }
+        container.appendChild(heading);
+    };
+
+    return {init, draw, checkEndGame, endGame, board};
+})();
+
+const game = (() => {
+    let winner;
+    let player1 = player('name', 'X');
+    let player2 = player('other name', 'O');
     let currentTurn = {
         number: 1,
         player: player1,
@@ -23,64 +66,42 @@ const game = () => {
                 currentTurn.player = player1;
             }
         }
-    };
+    }
 
-    const draw = (symbol, square) => {
-        square.textContent = symbol;
-    };
-
-    const endGame = (winningRow) => {
-        board.removeEventListener('click', makeMove);
-        let heading = document.createElement('H1');
-        let container = document.querySelector('div.container');
-        if (winner === undefined) {
-            heading.textContent = "Cat's game...";
-        } else {
-            heading.textContent = `${currentTurn.player.name} wins!`;
-        }
-        container.appendChild(heading);
-    };
-
-    const checkEndGame = () => {
-        let winPositions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+    const checkForDraw = () => {
         if (currentTurn.number > 9) {
-            console.log('game should be over');
-            endGame();
-        } else {
-            winPositions.forEach((array) => {
-                let values = [];
-                array.forEach((n) => {
-                    values.push(boardArray[n]);
-                });
-                if (values.every(x => x === 'X') || values.every(x => x === 'O')) {
-                    console.log('someone won');
-                    winner = currentTurn.player;
-                    endGame(array);
-                }
-            });
+            board.endGame(winner);
         }
     };
-   
-    const makeMove = (e) => {
-        let square = e.target;
 
-        if (square.textContent === "") {
-            let index = square.id
-            let symbol = currentTurn.player.symbol;
-
-            boardArray[index] = currentTurn.player.symbol;
-
-            draw(symbol, square);
-            currentTurn.number ++;
-            checkEndGame();
+    const postTurn = () => {
+        currentTurn.number ++;
+        checkForDraw();
+        console.log(board.checkEndGame());
+        if (board.checkEndGame()) {
+            winner = currentTurn.player;
+            board.endGame(winner); 
+        } else {
             currentTurn.toggle();
         }
     };
 
-    board.addEventListener('click', makeMove);
-    return {player1, player2, currentTurn};
-};
+    const makeMove = (e) => {
+        let square = e.target;
+        if (square.textContent === "") {
+            let symbol = currentTurn.player.symbol;
+            board.draw(symbol, square);
+            postTurn();
+        }
 
-let newGame = game();
+    };
 
-newGame;
+    const start = () => {
+        board.init();
+    };
+                
+    return {start, makeMove};
+})();
+
+
+game.start();
